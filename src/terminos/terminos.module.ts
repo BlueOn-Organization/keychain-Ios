e202b3c6d0c1dcf37dbc840eb3b414618a5c9b25
sebastianmayorga <j_25005@hotmail.com>
2018-12-12 21:36:24 -0500

    Se cambio para que el titulo aparezca en ingles y en español

--- src/terminos/terminos.module.ts
+++ src/terminos/terminos.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { TerminosPage } from './terminos';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { TerminosPage } from './terminos';
   ],
   imports: [
     IonicPageModule.forChild(TerminosPage),
+      TranslateModule
   ],
 })
 export class TerminosPageModule {}
